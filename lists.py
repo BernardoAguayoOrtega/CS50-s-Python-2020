a1a4862a5daba4a075b0651ac06735bcdf9d0f3a
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-04 12:35:50 -0600

    list => array

---
+++ lists.py
@@ -0,0 +1,17 @@
+# list of names
+names = ["harry", "Ron", "Herminone", "Ginny"]
+
+# print list of names
+print(names)
+
+## modify list
+names.append("Draco")
+
+# show again the list
+print(names)
+
+# sort the list
+names.sort()
+
+## show list sort it
+print(names)
