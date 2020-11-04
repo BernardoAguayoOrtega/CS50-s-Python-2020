c034bbd3171076705702611f70ac5269ef5eaef4
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-04 12:24:55 -0600

    tuple file

--- sequences02.py
+++ sequences02.py
@@ -2,3 +2,7 @@
 names = ["Harry", "Ron", "Hermanione"]
 
 print(names[0])
+
+numbers = [1,2,3,4]
+
+print(numbers)
---
+++ tuple.py
@@ -0,0 +1,6 @@
+numberA = 10.00
+numberB = 5.0
+
+cordinate = (numberA, numberB)
+
+print(cordinate)
