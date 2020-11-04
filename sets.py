b5cc6996271bb3e84ccb85605a6313f78d2c520b
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-04 12:47:00 -0600

    sets file => unique list of elements

---
+++ sets.py
@@ -0,0 +1,21 @@
+# create an empty set
+s = set()
+
+# Add elements to set
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(5)
+
+# print the values
+print(s)
+
+# remove element
+s.remove(1)
+
+# print again the set
+print(s)
+
+# know the length of the set
+print(f"the set has {len(s)} elements.")
