baabaf7566650ce5bb82420a0e1e1e97aeb355a2
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-05 11:49:44 -0600

    learn specific import and whole import

--- functions.py
+++ functions.py
@@ -1,5 +1,13 @@
 # functions
 from squares import square
+import squares
+
+print("---------------------------------------------")
 
 for i in range(10):
   print(f"the square of {i} is {square(i)}")
+
+print("---------------------------------------------")
+
+for i in range(10):
+  print(f"the square of {i} is {squares.square(i)}")
